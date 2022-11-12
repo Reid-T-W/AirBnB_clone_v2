0d1ace1edaccc9a20b8469d346f8737b7c2a185d
Reid-T-W <rediettadesse100@gmail.com>
2022-11-12 22:18:57 +0300

    Base inheritance added back

--- models/state.py
+++ models/state.py
@@ -5,7 +5,7 @@ from sqlalchemy import String, Column, ForeignKey
 from os import getenv
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
