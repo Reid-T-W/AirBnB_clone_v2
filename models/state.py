6ceb9897f2674e12242b607d386346077f43f25f
Reid-T-W <rediettadesse100@gmail.com>
2022-11-12 20:05:31 +0300

    init method added to state.py

--- models/state.py
+++ models/state.py
@@ -9,6 +9,9 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
 
+    def __init__(self):
+        pass
+
     @property
     def cities(self):
         """returns the corresponding cities of a state"""
